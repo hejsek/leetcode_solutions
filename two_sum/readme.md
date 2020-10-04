e482674e7e19f036412f3272a45cd1a8b45d36e6
heyduk2@seznam.cz <heyduk2@seznam.cz>
2020-10-04 18:53:13 +0200

    Added two sum problem solution

---
+++ two_sum/readme.md
@@ -0,0 +1,14 @@
+Commented solution for https://leetcode.com/problems/two-sum/ written in Typescript
+
+
+
+###### Problem description
+
+  Given an array of integers `nums` and an integer `target`, _return indices of the two numbers such that they add up to_ `target`.
+
+  You may assume that each input would have **exactly one solution**, and you may not use the _same_ element twice.
+  
+  You can return the answer in any order. 
+  
+  
+  
---
+++ two_sum/solution.ts
@@ -0,0 +1,20 @@
+﻿function twoSum(nums: number[], target: number): number[] {
+    // Initializatuon of new Map. Difference is used as a map key here and array index as a map value.
+    const map: Map<number, number> = new Map();
+
+    // Iteration through all numbers
+    for(let i = 0; i < nums.length; i++) {
+        // We check if difference was added to the Map in previous iterations
+        // If so our solution is our current i and value stored in Map where difference is used as a Map key
+        if(map.has(nums[i])) {
+            return [map.get(nums[i])!, i]
+        }
+
+        // We use difference after substraction of variable target and nums[i] on i index as a map key and actual loop counter(i) as a value
+        map.set(target-nums[i], i)
+
+    }
+
+
+    return []
+}
